2464c47128e08519ec3c9a55595ecd1b1d058632
Brendan Greene <brendanjgreene@gmail.com>
2016-11-28 12:37:50 +0000

    opdate route provider with new routes for nav bar elements"

--- js/app.js
+++ js/app.js
@@ -5,5 +5,25 @@ angular.module('BandApp').config(function($routeProvider){
 	$routeProvider.when('/', {
 		templateURL: 'templates/home.html',
 		controller: 'HomeController'
+	})
+	.when('/About_Us', {
+		templateURL: 'templates/About_Us.html',
+		controller: 'AboutUSControler'
+	})
+	.when('/Freebies', {
+		templateURL: 'templates/Freebies.html',
+		controller: 'FreebiesContoller'
+	})
+	.when('/Tracks', {
+		templateURL: 'templates/Tracks.html',
+		controller: 'TracksController'
+	})
+	.when('/Gigs', {
+		templateURL: 'templates/Gigs.html',
+		controller: 'GigsController'
+	})
+	.when('/Subscribe', {
+		templateURL: 'templates/Subscribe.html'
+		controller: 'SubscribeController'
 	});
 });

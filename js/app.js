1a1265347fb82a04737634d1826064b71dbde784
Nishant Kumar <nihantanu@gmail.com>
2016-12-15 15:46:58 +0530

    Fixed bower component issue

--- js/app.js
+++ js/app.js
@@ -1,31 +1,33 @@
-angular.module('BandApp', ['ngRoute', 'RouteControllers', 'h1Directive', 'UserService', 'angular-storage']);
-
+angular.module('BandApp', [
+    'ngRoute',
+    'angular-storage'
+]);
 mySong = "assets/audio/ImABeliever.mp3";
 
-angular.module('BandApp').config(function($routeProvider){
-	
+angular.module('BandApp').config(function($routeProvider) {
+
 	$routeProvider.when('/', {
-		templateUrl: 'templates/home.html',
-		controller: 'HomeController'
-	})
-	.when('/aboutUs', {
-		templateUrl: "templates/aboutUs.html",
-		controller: "aboutUsController"
-	})
-	.when('/freebies', {
-		templateUrl: "templates/freebies.html",
-		controller: "freebiesController"
-	})
-	.when('/tracks', {
-		templateUrl: "templates/tracks.html",
-		controller: "tracksController"
-	})
-	.when('/gigs', {
-		templateUrl: "templates/gigs.html",
-		controller: "gigsController"
-	})
-	.when('/subscribe', {
-		templateUrl: "templates/subscribe.html",
-		controller: "subscribeController"
-	});
+			templateUrl: 'templates/home.html',
+			controller: 'HomeController'
+		})
+		.when('/aboutUs', {
+			templateUrl: "templates/aboutUs.html",
+			controller: "aboutUsController"
+		})
+		.when('/freebies', {
+			templateUrl: "templates/freebies.html",
+			controller: "freebiesController"
+		})
+		.when('/tracks', {
+			templateUrl: "templates/tracks.html",
+			controller: "tracksController"
+		})
+		.when('/gigs', {
+			templateUrl: "templates/gigs.html",
+			controller: "gigsController"
+		})
+		.when('/subscribe', {
+			templateUrl: "templates/subscribe.html",
+			controller: "subscribeController"
+		});
 });

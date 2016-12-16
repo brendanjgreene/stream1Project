1998e291c86bc4dd645a615f3855736c3b35cc59
Brendan Greene <brendanjgreene@gmail.com>
2016-12-16 21:52:33 +0000

    finish logout contoller

--- js/controllers/logoutController.js
+++ js/controllers/logoutController.js
@@ -1,4 +1,10 @@
 angular.module('BandApp')
-    .controller('logoutController', function($scope) {
+    .controller('logoutController', function($scope, $location, $window, store) {
         $scope.title = "Do You Really want to Log Out!";
+        $scope.logOut = function(){
+        	$scope.authToken = store.remove('authToken');
+        	$scope.username = store.remove('username');
+        	$location.path("/");
+        	$window.location.reload();
+        }
     })

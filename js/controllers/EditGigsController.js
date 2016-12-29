ee93bd8f6eb07bd65c8de8c315fa6b087ea532cf
Brendan Greene <brendanjgreene@gmail.com>
2016-12-29 18:20:54 +0000

    coorections to make edit gigs controller agree with GigAPIService

--- js/controllers/EditGigsController.js
+++ js/controllers/EditGigsController.js
@@ -1,21 +1,25 @@
 angular.module('BandApp')
+
     .controller('EditGigsController', function($scope, $location, $routeParams, GigAPIService, store) {
         var id = $routeParams.id;
         var url = "https://morning-castle-91468.herokuapp.com/";
 
+        GigAPIService.getGigs(url + "todo/" + id, store.get('username'), store.get('authToken')).then(function(results) {
 
-        GigAPIService.getGigs(url + "todo/" + id, $scope.username, store.get('authToken')).then(function(results) {
-
-            $scope.gigs = results.data;
+            $scope.gig = results.data;
         }).catch(function(err) {
             console.log(err);
         });
  
         $scope.submitForm = function() {
-            if ($scope.edit-gigsForm.$valid) {
-                $scope.gig.username = $scope.username;
+            if ($scope.editGigForm.$valid) {
+                $scope.gig.username = store.get('username');
                 
-                GigAPIService.editGigs(url + "todo/" + id, $scope.gig, store.get('authToken')).then(function(results) {
+                GigAPIService.editGig(url + "todo/" + id, $scope.gig, store.get('authToken')).then(function(results) {
+                    //$scope.gig = results.data;
+                    //$scope.gigs = $scope.gigs.filter(function(element) {
+                    //    return element.id !==id;
+                    //});
                     $location.path("/gigs");
                 }).catch(function(err) {
                     console.log(err);

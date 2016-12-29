161bd0b5b5541c056401f802048a8ad11f9993fd
Brendan Greene <brendanjgreene@gmail.com>
2016-12-29 18:29:22 +0000

    experimenting with collapse

--- js/recollapse.js
+++ js/recollapse.js
@@ -1,27 +1,26 @@
-$("#myNavbar").on('click', function(event){
-    var $clickedOn = $(event.target),
-    $collapsableItems = $('.collapse'),
-    isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
-    isLink = ($clickedOn.closest('a').length == 1),
-    isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
-  
-    if( (!isToggleButton && isLink) || isOutsideNavbar ) {
-      $collapsableItems.each(function(){
-        $(this).collapse('hide');
-      });
-    }
-  })
+/*$(document.body.children[3]).on('click', function(event){
+  var $clickedOn = $(event.target),
+  $collapsableItems = $('.collapse'),
+  isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+  isLink = ($clickedOn.closest('a').length == 1),
+  isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
 
-$(document.).on('click', function(event){
-    var $clickedOn = $(event.target),
-    $collapsableItems = $('.collapse'),
-    isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
-    isLink = ($clickedOn.closest('a').length == 1),
-    isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
-  
-    if( (!isToggleButton && isLink) || isOutsideNavbar ) {
-      $collapsableItems.each(function(){
-        $("#myAbout").collapse('hide');
-      });
-    }
-  });
+  if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+    $collapsableItems.each(function(){
+      $(this).collapse('hide');
+    });
+  }
+});*/
+$($location.path("/aboutUs")).on('click', function(event){
+  var $clickedOn = $(event.target),
+  $collapsableItems = $('.collapse'),
+  isToggleButton = ($clickedOn.closest('.navbar-toggle').length == 1),
+  isLink = ($clickedOn.closest('a').length == 1),
+  isOutsideNavbar = ($clickedOn.parents('.navbar').length == 0);
+
+  if( (!isToggleButton && isLink) || isOutsideNavbar ) {
+    $collapsableItems.each(function(){
+      $(this).collapse('hide');
+    });
+  }
+});

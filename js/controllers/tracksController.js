b8150a93adad3041bfcc74b10adf7ba2e4801f3b
Brendan Greene <brendanjgreene@gmail.com>
2016-12-29 19:48:41 +0000

    toggle glyphicon on click

--- js/controllers/tracksController.js
+++ js/controllers/tracksController.js
@@ -1,4 +1,7 @@
 angular.module('BandApp')
     .controller('tracksController', function($scope){
     	$scope.title = "Tracks";
+    	$('tr').click(function(){
+    		$(this).find('span').toggleClass('glyphicon-chevron-down').toggleClass('glyphicon-chevron-up');
+    	});
     })

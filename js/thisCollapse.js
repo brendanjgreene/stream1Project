d4d9ec9b604a244c51b642e372ff4b9ed53e6fc1
Brendan Greene <brendanjgreene@gmail.com>
2016-12-29 18:37:05 +0000

    create this collapse script

---
+++ js/thisCollapse.js
@@ -0,0 +1,9 @@
+$('.pleasework').click(function() {
+        $('.navbar-toggle:visible').click(),
+        $('.clickable:visible').click();
+    });
+body > div.pleasework
+body > div.container > div > div.pleasework.ng-scope
+/html/body/div[3]/div/div[2]
+body > div.pleasework
+/html/body/div[4]
